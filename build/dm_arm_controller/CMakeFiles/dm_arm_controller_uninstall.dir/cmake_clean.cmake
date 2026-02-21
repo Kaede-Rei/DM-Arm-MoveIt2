fc3f4c8cd7bea96c82d76c8d9fe88706f0ff295c
kaede-rei <kaerei86@gmail.com>
2026-02-21 23:46:35 +0800

    chore: 清理工作区杂项

--- build/dm_arm_controller/CMakeFiles/dm_arm_controller_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/dm_arm_controller_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/dm_arm_controller_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/dm_arm_description/CMakeFiles/dm_arm_description_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/dm_arm_description_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/dm_arm_description_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
